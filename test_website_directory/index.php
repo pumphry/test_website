8182c5aa80bfc926fd07c9bd5a420ce05012ecb1
Alexander John Murphy <alexjmurphy90@gmail.com>
2016-07-26 01:49:27 -0400

    Created a project directory. Moved Bootstrap source files to the library folder location.

--- test_website_directory/index.php
+++ test_website_directory/index.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <title>test website index</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- Optional Bootstrap theme -->
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 </head>
 <body>
 <?php echo '<p>INDEX PAGE</p>'; ?>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>

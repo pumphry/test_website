4d5f5e6ef27055ac6ff98467c3a69cd57cbc95bb
Alexander John Murphy <alexjmurphy90@gmail.com>
2016-07-26 18:15:20 -0400

    Created a  home html page and created javascript scripts for both home and index html pages to use. Created a redirect function that gets called in the index page that sends the view to the home page right away.

---
+++ test_website_directory/public_html/js/index.js
@@ -0,0 +1,5 @@
+// This is the index page's JavaScript script. 
+
+function redirectToHomePage() {
+	window.location.pathname = "test_website/test_website_directory/test_website_directory/public_html/home.html";
+}
